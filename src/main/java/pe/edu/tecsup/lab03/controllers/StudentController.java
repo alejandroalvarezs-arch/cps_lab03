13d21124517b7f09bfa24478f86b3a52c4e4eede
valeria <valeriaapazacu@gmail.com>
2026-09-06 14:02:02 -0500

    Modifica clases Student en sprint-2

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -5,4 +5,8 @@ public class StudentController {
     public void controller() {
         // TO DO
     }
+
+    public void deleteStudent(Long id) {
+        // Sprint 2: Endpoint para eliminar alumno
+    }
 }
--- src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -1,4 +1,14 @@
 package pe.edu.tecsup.lab03.entities;
 
 public class StudentEntity {
+
+    private String carrera;
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
 }
